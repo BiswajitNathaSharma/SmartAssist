72e5be82719714ce64c12b5e88654d4331efe16d
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-04-23 17:36:05 +0530

    new  stop icon added

---
+++ src/components/Help/Help.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./help.css"
+function Help() {
+    return (
+        <div className='help'>
+            <p>help me</p>
+        </div>
+    )
+}
+
+export default Help
